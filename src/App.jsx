327dcf719b1d8f747409d46e2f0a184817a07585
trocha <thiagorocha841@gmail.com>
2025-11-11 14:55:49 -0300

    fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  
+  const API_URL = import.meta.env.VITE_API_URL;
+  const [games, setGames] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchGames = async () => {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    setGames(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchGames();
+  }, []);
+
   return (
     <>
       <>
@@ -12,26 +27,67 @@ function App() {
 
             <nav className="nav">
               <ul>
-                <li><a href="#" className="nav-link">Inicio</a></li>
-                <li><a href="#" className="nav-link">En Juego</a></li>
-                <li><a href="#" className="nav-link">Proximos</a></li>
+                <li>
+                  <a href="#" className="nav-link">
+                    Inicio
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="nav-link">
+                    En Juego
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="nav-link">
+                    Proximos
+                  </a>
+                </li>
               </ul>
             </nav>
           </section>
         </header>
+        <main>
+          <aside className="aside">
+            <h3 className="section-title">Principales competencias</h3>
 
-        <aside className="aside">
-          <h3 className="section-title">Principales competencias</h3>
-
-          <nav>
-            <ul className="leagues">
-              <li><a href="#" className="side-item">⚽ Champions League</a></li>
-              <li><a href="#" className="side-item">⚽ Premier League</a></li>
-              <li><a href="#" className="side-item">⚽ La Liga</a></li>
-              <li><a href="#" className="side-item">⚽ LPF</a></li>
-            </ul>
-          </nav>
-        </aside>  
+            <nav>
+              <ul className="leagues">
+                <li>
+                  <a href="#" className="side-item">
+                    ⚽ Champions League
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="side-item">
+                    ⚽ Premier League
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="side-item">
+                    ⚽ La Liga
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="side-item">
+                    ⚽ LPF
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </aside>
+          {loading ? (
+            <div className="loading">Cargando juegos...</div>
+          ) : (
+            <section className="section-games">
+              {games.leagues.map((league) => (
+                <div key={league.id}>
+                  <h2>{league.name}</h2>
+                  <p>{league.country_name}</p>
+                </div>
+              ))}
+            </section>
+          )}
+        </main>
       </>
     </>
   );
