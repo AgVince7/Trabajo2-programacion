471d44fa44625bce7c693916088f78c20504294b
Avin <avince@frsn.utn.edu.ar>
2025-11-11 14:05:19 -0300

    Segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,12 @@
 import "./App.css";
+import { Aside } from "./components/Aside";
+import { Header } from "./components/header";
 
 function App() {
-  
   return (
     <>
-      <>
-        <header className="header">
-          <section className="logo-area">
-            <img src="./public/logo.png" alt="logo" />
-            <h1 className="logo-text">FútbolAlInstante</h1>
-
-            <nav className="nav">
-              <ul>
-                <li><a href="#" className="nav-link">Inicio</a></li>
-                <li><a href="#" className="nav-link">En Juego</a></li>
-                <li><a href="#" className="nav-link">Proximos</a></li>
-              </ul>
-            </nav>
-          </section>
-        </header>
-
-        <aside className="aside">
-          <h3 className="section-title">Principales competencias</h3>
-
-          <nav>
-            <ul className="leagues">
-              <li><a href="#" className="side-item">⚽ Champions League</a></li>
-              <li><a href="#" className="side-item">⚽ Premier League</a></li>
-              <li><a href="#" className="side-item">⚽ La Liga</a></li>
-              <li><a href="#" className="side-item">⚽ LPF</a></li>
-            </ul>
-          </nav>
-        </aside>  
-      </>
+      <Header />
+      <Aside />
     </>
   );
 }
