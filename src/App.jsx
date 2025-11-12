7a1a75be13d4853b3c000b2f770ad4a2a9d06951
trocha <thiagorocha841@gmail.com>
2025-11-12 16:26:37 -0300

    resultados agregados

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,11 @@ function App() {
   const API_URL = import.meta.env.VITE_API_URL;
   const [leagues, setLeagues] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [boolTime, setBoolTime] = useState(false);
+
+  setTimeout(() => {
+    setBoolTime(!boolTime);
+  }, 10000);
 
   const fetchGames = async () => {
     const response = await fetch(API_URL);
@@ -17,7 +22,7 @@ function App() {
 
   useEffect(() => {
     fetchGames();
-  }, []);
+  }, [boolTime]);
 
   return (
     <>
--- src/components/CardsPartidos.jsx
+++ src/components/CardsPartidos.jsx
@@ -1,16 +1,37 @@
 export const CardsPartidos = (partido) => {
   const urlImagen = `https://api.promiedos.com.ar/images/team/${partido.partido.teams[0].id}/1`;
   const urlImagen2 = `https://api.promiedos.com.ar/images/team/${partido.partido.teams[1].id}/1`;
+
   return (
     <section className="cards-partidos">
-      <span className="hora">{partido.partido.game_time_to_display}</span>
+      <span className="hora">
+        {partido.partido.status.enum === 1
+          ? partido.partido.start_time
+          : partido.partido.game_time_to_display}
+        {partido.partido.status.enum === 3 && partido.partido.status.short_name}
+      </span>
 
       <div className="equipos">
-        <span>{partido.partido.teams[0].name}</span>
-        <img src={urlImagen} alt="Escudo" />
-        <span className="versus"> - </span>
-        <img src={urlImagen2} alt="Escudo" />
-        <span>{partido.partido.teams[1].name}</span>
+        <div className="equipo1">
+          <span>{partido.partido.teams[0].name}</span>
+          <img src={urlImagen} alt="Escudo" />
+        </div>
+
+        <div>
+          {partido.partido.status.enum !== 1 && (
+            <span>{partido.partido.scores[0]}</span>
+          )}
+
+          <span className="versus"> - </span>
+          {partido.partido.status.enum !== 1 && (
+            <span>{partido.partido.scores[1]}</span>
+          )}
+        </div>
+
+        <div className="equipo2">
+          <img src={urlImagen2} alt="Escudo" />
+          <span>{partido.partido.teams[1].name}</span>
+        </div>
       </div>
     </section>
   );
